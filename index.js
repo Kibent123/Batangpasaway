527acfb32ea8810c2d4218bc49904b9cbe88dae0
Kibent123 <67850211+Kibent123@users.noreply.github.com>
2020-07-21 15:24:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -271,7 +271,7 @@ client.on('guildMemberAdd', member => {
 });
 
 client.on('guildMemberRemove', member => {
-    const genchatChannel = member.guild.channels.cache.find(ch => ch.id === mgaChannelID);
+    const mgaChannel = member.guild.channels.cache.find(ch => ch.id === mgaChannelID);
     const messageEmbed = new MessageEmbed();
     if (genchatChannel) {
         messageEmbed.setTitle(`Babye! Balik ka next time welcome ka parin!`);
@@ -280,7 +280,7 @@ client.on('guildMemberRemove', member => {
         messageEmbed.setThumbnail("https://media.discordapp.net/attachments/731061289744334868/731129635349463040/123s.png");
         messageEmbed.setImage(member.user.displayAvatarURL());
         messageEmbed.setAuthor("BP-Bot", "https://media.discordapp.net/attachments/731061289744334868/731129635349463040/123s.png", "https://discord.gg/7Mz6g6");
-        genchatChannel.send(messageEmbed);
+        mgaChannel.send(messageEmbed);
     }
 });
 
